0695aa61f24303f5ee9a078baa90c9c6404e9b87
yifei325325 <yifei1193@163.com>
2015-08-27 17:50:59 +0800

    V1.2基本功能完成

--- Guess/guess.py
+++ Guess/guess.py
@@ -17,12 +17,24 @@ class MyApp(wx.App):
         self.temp_rock = rock_image.ConvertToBitmap()
         paper_image = wx.Image('paper.gif', wx.BITMAP_TYPE_GIF)
         self.temp_paper = paper_image.ConvertToBitmap()
+#         self.temp_paper.
+        
+        win_image = wx.Image("win.jpg",wx.BITMAP_TYPE_JPEG)
+        self.temp_win = win_image.ConvertToBitmap()
+        lose_image = wx.Image("lose.jpg",wx.BITMAP_TYPE_JPEG)
+        self.temp_lose = lose_image.ConvertToBitmap()
+        dogfall_image = wx.Image("dogfall.jpg",wx.BITMAP_TYPE_JPEG)
+        self.temp_dogfall = dogfall_image.ConvertToBitmap()
+#         self.temp_dogfall.
         
         self.panel_main = wx.Panel(frame,size=frame.GetSize())
+        self.panel_main.SetBackgroundColour("white")
+#         self.panel_main.SetForegroundColour("white")
+#         self.panel_main.SetBackgroundColour("white")
         
-        self.panel_com_scissors = wx.Panel(frame,size=(self.temp_scissors.GetWidth(), self.temp_scissors.GetHeight()),pos=(self.temp_scissors.GetWidth()+200,30))
-        self.panel_com_rock = wx.Panel(frame,size=(self.temp_scissors.GetWidth(), self.temp_scissors.GetHeight()),pos=(self.temp_scissors.GetWidth()+200,30))
-        self.panel_com_paper = wx.Panel(frame,size=(self.temp_scissors.GetWidth(), self.temp_scissors.GetHeight()),pos=(self.temp_scissors.GetWidth()+200,30))
+        self.panel_com_scissors = wx.Panel(frame,size=(self.temp_scissors.GetWidth(), self.temp_scissors.GetHeight()),pos=(self.temp_scissors.GetWidth()+180,30))
+        self.panel_com_rock = wx.Panel(frame,size=(self.temp_scissors.GetWidth(), self.temp_scissors.GetHeight()),pos=(self.temp_scissors.GetWidth()+180,30))
+        self.panel_com_paper = wx.Panel(frame,size=(self.temp_scissors.GetWidth(), self.temp_scissors.GetHeight()),pos=(self.temp_scissors.GetWidth()+180,30))
         
         self.panel_player_scissors = wx.Panel(frame,size = (self.temp_scissors.GetWidth(), self.temp_scissors.GetHeight()) ,pos=(60,30))
         self.panel_player_rock = wx.Panel(frame,size = (self.temp_rock.GetWidth(), self.temp_rock.GetHeight()) ,pos=(60,30))
@@ -40,6 +52,14 @@ class MyApp(wx.App):
         rock_button.Bind(wx.EVT_BUTTON, self.rock_button_press)
         paper_button.Bind(wx.EVT_BUTTON, self.paper_button_press)
         
+        self.panel_player_result = wx.Panel(frame,size =(self.temp_win.GetWidth(),self.temp_win.GetHeight()),pos =(85,170))
+        self.panel_com_result = wx.Panel(frame,size =(self.temp_win.GetWidth(),self.temp_win.GetHeight()),pos =(335,170))
+        
+        self.win_play = 0
+        self.lose_play = 0
+        self.dogfall_play = 0
+        self.result_play = wx.StaticText(self.panel_main,size = (150,20),pos=(87,260),label=u"胜:%d 负:%d 平:%d"%(self.win_play,self.lose_play,self.dogfall_play))
+        self.result_com = wx.StaticText(self.panel_main,size = (150,20),pos=(337,260),label=u"胜:%d 负:%d 平:%d"%(self.lose_play,self.win_play,self.dogfall_play))
         frame.Show()
         
         return 1
@@ -51,22 +71,47 @@ class MyApp(wx.App):
             self.panel_com_scissors.Show(True)
             self.bmp = wx.StaticBitmap(parent=self.panel_com_scissors, bitmap=self.temp_scissors)
             if flag == 0:
-                print u"平局"
+                self.dogfall_play += 1
+                self.result_play = wx.StaticText(self.panel_main,size = (150,20),pos=(87,260),label=u"胜:%d 负:%d 平:%d"%(self.win_play,self.lose_play,self.dogfall_play))
+                self.result_com = wx.StaticText(self.panel_main,size = (150,20),pos=(337,260),label=u"胜:%d 负:%d 平:%d"%(self.lose_play,self.win_play,self.dogfall_play))
+                self.bmp = wx.StaticBitmap(parent=self.panel_player_result,bitmap=self.temp_dogfall)
+                self.bmp = wx.StaticBitmap(parent=self.panel_com_result,bitmap=self.temp_dogfall)
+                
             elif flag ==1:
-                print u"你赢了"
+                self.win_play += 1
+                self.result_play = wx.StaticText(self.panel_main,size = (150,20),pos=(87,260),label=u"胜:%d 负:%d 平:%d"%(self.win_play,self.lose_play,self.dogfall_play))
+                self.result_com = wx.StaticText(self.panel_main,size = (150,20),pos=(337,260),label=u"胜:%d 负:%d 平:%d"%(self.lose_play,self.win_play,self.dogfall_play))
+                self.bmp = wx.StaticBitmap(parent=self.panel_player_result,bitmap=self.temp_win)
+                self.bmp = wx.StaticBitmap(parent=self.panel_com_result,bitmap=self.temp_lose)
             elif flag == 2:
-                print u"你输了"
+                self.lose_play += 1
+                self.result_play = wx.StaticText(self.panel_main,size = (150,20),pos=(87,260),label=u"胜:%d 负:%d 平:%d"%(self.win_play,self.lose_play,self.dogfall_play))
+                self.result_com = wx.StaticText(self.panel_main,size = (150,20),pos=(337,260),label=u"胜:%d 负:%d 平:%d"%(self.lose_play,self.win_play,self.dogfall_play))
+                self.bmp = wx.StaticBitmap(parent=self.panel_player_result,bitmap=self.temp_lose)
+                self.bmp = wx.StaticBitmap(parent=self.panel_com_result,bitmap=self.temp_win)
         elif choice == 1:
             self.panel_com_rock.Show(True)
             self.panel_com_paper.Show(False)
             self.panel_com_scissors.Show(False)
             self.bmp = wx.StaticBitmap(parent=self.panel_com_rock, bitmap=self.temp_rock)
             if flag == 0:
-                print u"你输了"
+                self.lose_play += 1
+                self.result_play = wx.StaticText(self.panel_main,size = (150,20),pos=(87,260),label=u"胜:%d 负:%d 平:%d"%(self.win_play,self.lose_play,self.dogfall_play))
+                self.result_com = wx.StaticText(self.panel_main,size = (150,20),pos=(337,260),label=u"胜:%d 负:%d 平:%d"%(self.lose_play,self.win_play,self.dogfall_play))
+                self.bmp = wx.StaticBitmap(parent=self.panel_player_result,bitmap=self.temp_lose)
+                self.bmp = wx.StaticBitmap(parent=self.panel_com_result,bitmap=self.temp_win)
             elif flag ==1:
-                print u"平局"
+                self.dogfall_play += 1
+                self.result_play = wx.StaticText(self.panel_main,size = (150,20),pos=(87,260),label=u"胜:%d 负:%d 平:%d"%(self.win_play,self.lose_play,self.dogfall_play))
+                self.result_com = wx.StaticText(self.panel_main,size = (150,20),pos=(337,260),label=u"胜:%d 负:%d 平:%d"%(self.lose_play,self.win_play,self.dogfall_play))
+                self.bmp = wx.StaticBitmap(parent=self.panel_player_result,bitmap=self.temp_dogfall)
+                self.bmp = wx.StaticBitmap(parent=self.panel_com_result,bitmap=self.temp_dogfall)
             elif flag == 2:
-                print u"你赢了"
+                self.win_play += 1
+                self.result_play = wx.StaticText(self.panel_main,size = (150,20),pos=(87,260),label=u"胜:%d 负:%d 平:%d"%(self.win_play,self.lose_play,self.dogfall_play))
+                self.result_com = wx.StaticText(self.panel_main,size = (150,20),pos=(337,260),label=u"胜:%d 负:%d 平:%d"%(self.lose_play,self.win_play,self.dogfall_play))
+                self.bmp = wx.StaticBitmap(parent=self.panel_player_result,bitmap=self.temp_win)
+                self.bmp = wx.StaticBitmap(parent=self.panel_com_result,bitmap=self.temp_lose)
                 
         elif choice == 2:
             self.panel_com_rock.Show(False)
@@ -74,11 +119,23 @@ class MyApp(wx.App):
             self.panel_com_paper.Show(True)
             self.bmp = wx.StaticBitmap(parent=self.panel_com_paper, bitmap=self.temp_paper)
             if flag == 0:
-                print u"你赢了"
+                self.win_play += 1
+                self.result_play = wx.StaticText(self.panel_main,size = (150,20),pos=(87,260),label=u"胜:%d 负:%d 平:%d"%(self.win_play,self.lose_play,self.dogfall_play))
+                self.result_com = wx.StaticText(self.panel_main,size = (150,20),pos=(337,260),label=u"胜:%d 负:%d 平:%d"%(self.lose_play,self.win_play,self.dogfall_play))
+                self.bmp = wx.StaticBitmap(parent=self.panel_player_result,bitmap=self.temp_win)
+                self.bmp = wx.StaticBitmap(parent=self.panel_com_result,bitmap=self.temp_lose)
             elif flag ==1:
-                print u"你输了"
+                self.lose_play += 1
+                self.result_play = wx.StaticText(self.panel_main,size = (150,20),pos=(87,260),label=u"胜:%d 负:%d 平:%d"%(self.win_play,self.lose_play,self.dogfall_play))
+                self.result_com = wx.StaticText(self.panel_main,size = (150,20),pos=(337,260),label=u"胜:%d 负:%d 平:%d"%(self.lose_play,self.win_play,self.dogfall_play))
+                self.bmp = wx.StaticBitmap(parent=self.panel_player_result,bitmap=self.temp_lose)
+                self.bmp = wx.StaticBitmap(parent=self.panel_com_result,bitmap=self.temp_win)
             elif flag == 2:
-                print u"平局"
+                self.dogfall_play += 1
+                self.result_play = wx.StaticText(self.panel_main,size = (150,20),pos=(87,260),label=u"胜:%d 负:%d 平:%d"%(self.win_play,self.lose_play,self.dogfall_play))
+                self.result_com = wx.StaticText(self.panel_main,size = (150,20),pos=(337,260),label=u"胜:%d 负:%d 平:%d"%(self.lose_play,self.win_play,self.dogfall_play))
+                self.bmp = wx.StaticBitmap(parent=self.panel_player_result,bitmap=self.temp_dogfall)
+                self.bmp = wx.StaticBitmap(parent=self.panel_com_result,bitmap=self.temp_dogfall)
         
             
             
