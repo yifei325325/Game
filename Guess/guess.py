6f2f6be3feded2f7d66a12dbc0679b2e5bf2b3e7
yifei325325 <yifei1193@163.com>
2015-08-27 09:49:02 +0800

    V1.1 修改图片大小

--- Guess/guess.py
+++ Guess/guess.py
@@ -10,21 +10,24 @@ import random
 class MyApp(wx.App):
     def OnInit(self):
         frame = wx.Frame(parent=None,title="Game",size=(500,400),style=wx.MINIMIZE_BOX|wx.CLOSE_BOX|wx.SYSTEM_MENU|wx.CAPTION)
+        
         scissors_image = wx.Image('scissors.gif', wx.BITMAP_TYPE_GIF)
         self.temp_scissors = scissors_image.ConvertToBitmap()
         rock_image = wx.Image('rock.gif', wx.BITMAP_TYPE_GIF)
         self.temp_rock = rock_image.ConvertToBitmap()
         paper_image = wx.Image('paper.gif', wx.BITMAP_TYPE_GIF)
         self.temp_paper = paper_image.ConvertToBitmap()
+        
         self.panel_main = wx.Panel(frame,size=frame.GetSize())
+        
         self.panel_com_scissors = wx.Panel(frame,size=(self.temp_scissors.GetWidth(), self.temp_scissors.GetHeight()),pos=(self.temp_scissors.GetWidth()+200,30))
         self.panel_com_rock = wx.Panel(frame,size=(self.temp_scissors.GetWidth(), self.temp_scissors.GetHeight()),pos=(self.temp_scissors.GetWidth()+200,30))
         self.panel_com_paper = wx.Panel(frame,size=(self.temp_scissors.GetWidth(), self.temp_scissors.GetHeight()),pos=(self.temp_scissors.GetWidth()+200,30))
+        
         self.panel_player_scissors = wx.Panel(frame,size = (self.temp_scissors.GetWidth(), self.temp_scissors.GetHeight()) ,pos=(60,30))
         self.panel_player_rock = wx.Panel(frame,size = (self.temp_rock.GetWidth(), self.temp_rock.GetHeight()) ,pos=(60,30))
         self.panel_player_paper = wx.Panel(frame,size = (self.temp_paper.GetWidth(), self.temp_paper.GetHeight()) ,pos=(60,30))
         
-          
         bmp_scissors_1 = wx.Image("scissors_1.gif", wx.BITMAP_TYPE_GIF).ConvertToBitmap()
         bmp_rock_1 = wx.Image("rock_1.gif", wx.BITMAP_TYPE_GIF).ConvertToBitmap()
         bmp_paper_1 = wx.Image("paper_1.gif", wx.BITMAP_TYPE_GIF).ConvertToBitmap()
@@ -32,11 +35,12 @@ class MyApp(wx.App):
         scissors_button = wx.BitmapButton(self.panel_main,-1, bmp_scissors_1,size=(50,50),pos=(30,300),style=wx.BU_AUTODRAW)
         rock_button = wx.BitmapButton(self.panel_main,-1, bmp_rock_1,size=(50,50),pos=(100,300))
         paper_button = wx.BitmapButton(self.panel_main,-1, bmp_paper_1,size=(50,50),pos=(170,300))
+        
         scissors_button.Bind(wx.EVT_BUTTON, self.scissors_button_press)
         rock_button.Bind(wx.EVT_BUTTON, self.rock_button_press)
         paper_button.Bind(wx.EVT_BUTTON, self.paper_button_press)
-        frame.Show()
         
+        frame.Show()
         
         return 1
     def Start(self,flag):
